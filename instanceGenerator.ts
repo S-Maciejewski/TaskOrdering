a7bd75f99f04223d0ae6c21086b3471cacaa184d
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-10-10 22:26:44 +0200

    Libs setup, argv and fs in generator

--- instanceGenerator.ts
+++ instanceGenerator.ts
@@ -1,13 +1,15 @@
+import * as fs from 'fs';
 
 function generateInstance(size: number, timeRange = 10, CPUs = 4) {
-    console.log(size);
+    let instance = `${size}\n`;
+
     for (let i = 0; i < size; i++) {
-        const p = Math.floor(Math.random() * timeRange);
+        const p = Math.floor(Math.random() * timeRange) + 1;
         const r = Math.floor(Math.random() * timeRange / CPUs * i);
         const d = r + p + Math.floor(Math.random() * timeRange);
-
-        console.log(p, r, d);
+        instance += `${p} ${r} ${d}\n`;
     }
+    fs.writeFileSync('./instance.txt', instance);
 }
 
-generateInstance(10);
+generateInstance(process.argv[2] as unknown as number);
