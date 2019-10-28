3296e09d0e64d20c4b83f2b022bf9c10f52d377f
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-10-28 23:03:21 +0100

    Solution validator

--- instanceGenerator.ts
+++ instanceGenerator.ts
@@ -1,12 +1,13 @@
 import * as fs from 'fs';
 
 function generateInstance(size: number, fileName: string, timeRange = 10,
-    buffer = 6, CPUs = 4) {
+    buffer = 8, CPUs = 4) {
     let instance = `${size}\n`;
 
     for (let i = 0; i < size; i++) {
         const p = Math.floor(Math.random() * timeRange) + 1;
-        const r = Math.floor((Math.random() + 1) * timeRange / CPUs * i);
+        // const r = Math.floor((Math.random()) * timeRange / CPUs * i);
+        const r = Math.floor((Math.random()) * timeRange / CPUs + i / CPUs);
         const d = r + p + Math.floor(Math.random() * buffer) + 1;
         instance += `${p} ${r} ${d}\n`;
     }
@@ -14,5 +15,12 @@ function generateInstance(size: number, fileName: string, timeRange = 10,
     fs.writeFileSync(fileName, instance);
 }
 
-generateInstance(process.argv[2] as unknown as number,
-    process.argv.length > 3 ? process.argv[3] : 'instance.txt');
+if (process.argv.length > 2) {
+    generateInstance(process.argv[2] as unknown as number,
+        process.argv.length > 3 ? process.argv[3] : 'instance.txt');
+} else {
+    for (let i = 50; i <= 500; i += 50) {
+        generateInstance(i, `./instances/instance${i}.txt`);
+    }
+}
+
--- validator.ts
+++ validator.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 
 class Task {
+    public id: number; // Just for ease of handling tasks
     public p: number;  // Time to complete
     public r: number;  // Ready
     public d: number;  // Deadline
@@ -14,14 +15,59 @@ class Task {
 
 let tasks: Task[];
 let instanceSize: number;
+let solution: any;
+let totalPenalty: number;
 
 function loadInstance(fileName: string) {
     const data = fs.readFileSync(fileName, 'utf-8');
     instanceSize = data.split('\n')[0] as unknown as number;
+    let taskID = 1;
 
     tasks = data.split('\n').slice(1, data.split('\n').length - 1)
         .map(row => new Task(...row.split(' ')
-            .map(val => val as unknown as number)));
+            .map(val => val as unknown as number * 1)))
+        .map(task => ({ ...task, id: taskID++ }));
+}
+
+function loadSolution(fileName: string) {
+    const data = fs.readFileSync(fileName, 'utf-8');
+    totalPenalty = data.split('\n')[0] as unknown as number;
+
+    solution = data.split('\n').slice(1).map(obj => obj.split(' '))
+        .map(coreTasks => coreTasks.filter(val => val !== '')
+            .map(val => val as unknown as number * 1));
+}
+
+function getCorePenalty(coreTasks: Task[]) {
+    let time = 0;
+    let penalty = 0;
+
+    coreTasks.map(task => {
+        if (task.r > time) {
+            time = task.r + task.p;
+        } else {
+            time += task.p;
+        }
+
+        if (time > task.d) {
+            penalty += time - task.d;
+        }
+        // Debug
+        // console.log('\nTask:', task, '\n\tTime:', time, '\n\tPenalty:', penalty);
+    });
+    return penalty;
+}
+
+function calculatePenalty() {
+    return solution
+        .map(core =>
+            getCorePenalty(tasks.filter(task => core.includes(task.id))))
+        .reduce((a, b) => a + b, 0);
 }
 
 loadInstance(process.argv.length > 2 ? process.argv[2] : 'instance.txt');
+loadSolution(process.argv.length > 3 ? process.argv[3] : 'solution.txt');
+
+console.log('Calculated penalty:', calculatePenalty(), '\nSolution penalty:', totalPenalty);
+// console.log(tasks);
+// console.log(solution);
